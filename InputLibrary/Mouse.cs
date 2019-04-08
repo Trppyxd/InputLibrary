bc7535fa85d514469445f16f656ca7720d67eb96
Trppyxd <33310226+Trppyxd@users.noreply.github.com>
2019-04-08 21:28:23 +0300

    +Helper, +MMouse Func, +Cursor DLL imports

--- InputLibrary/Mouse.cs
+++ InputLibrary/Mouse.cs
@@ -7,14 +7,50 @@ namespace InputLibrary
     {
         #region DLL Imports
 
+        /// <summary>
+        /// Mouse event used to manipulate the mouse.
+        /// </summary>
+        /// <param name="dwFlags"> See <see cref="MouseEventFlags"/> for possible flags. </param>
+        /// <param name="dx"></param>
+        /// <param name="dy"></param>
+        /// <param name="dwData"></param>
+        /// <param name="dwExtraInfo"></param>
+        /// <seealso cref="LeftClick"/>
+        /// <seealso cref="LeftDown"/>
+        /// <seealso cref="LeftUp"/>
         [DllImport("user32.dll")]
-        static extern void mouse_event(uint dwFlags, int dx, int dy, uint dwData, int dwExtraInfo);
+        static extern void mouse_event(int dwFlags, int dx, int dy, uint dwData, int dwExtraInfo);
+
+        #region Cursor
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetCursorPos(out POINT lpPoint);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public POINT(int X, int Y)
+            {
+                this.X = X;
+                this.Y = Y;
+            }
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetCursorPos(int X, int Y);
+
+        #endregion
 
         #endregion
 
         #region Flags
         [Flags]
-        public enum MouseEventFlags : uint
+        public enum MouseEventFlags
         {
             ABSOLUTE = 0x8000,
             LEFTDOWN = 0x0002,
@@ -30,7 +66,8 @@ namespace InputLibrary
             HWHEEL = 0x01000
         }
 
-        public enum MouseEventDataXButtons : uint
+        [Flags]
+        public enum MouseEventDataXButtons
         {
             XBUTTON1 = 0x00000001,
             XBUTTON2 = 0x00000002
@@ -40,31 +77,77 @@ namespace InputLibrary
 
         #region Functions
 
-        public static void Click()
+        #region Helpers
+
+        private static void DoMouseAction(MouseEventFlags flag)
         {
-            mouse_event((int)MouseEventFlags.LEFTDOWN, 0, 0, 0, 0);
-            mouse_event((int)MouseEventFlags.LEFTUP, 0, 0, 0, 0);
+            if (!GetCursorPos(out POINT pos))
+                throw new InvalidOperationException("Could not retrieve mouse position.");
+
+            mouse_event((int)flag, pos.X, pos.Y, 0, 0);
+        }
+
+        #endregion
+
+        #region LeftMouse
+
+        public static void LeftClick()
+        {
+            DoMouseAction(MouseEventFlags.LEFTDOWN);
+            DoMouseAction(MouseEventFlags.LEFTUP);
         }
 
         public static void LeftDown()
         {
-            mouse_event((int)MouseEventFlags.LEFTDOWN, 0, 0, 0, 0);
+            DoMouseAction(MouseEventFlags.LEFTDOWN);
         }
 
         public static void LeftUp()
         {
-            mouse_event((int)MouseEventFlags.LEFTUP, 0, 0, 0, 0);
+            DoMouseAction(MouseEventFlags.LEFTUP);
+        }
+
+        #endregion
+
+        #region RightMouse
+
+        public static void RightClick()
+        {
+            DoMouseAction(MouseEventFlags.RIGHTDOWN);
+            DoMouseAction(MouseEventFlags.RIGHTUP);
         }
 
         public static void RightDown()
         {
-            mouse_event((int)MouseEventFlags.RIGHTDOWN, 0, 0, 0, 0);
+            DoMouseAction(MouseEventFlags.RIGHTDOWN);
         }
 
         public static void RightUp()
         {
-            mouse_event((int)MouseEventFlags.RIGHTUP, 0, 0, 0, 0);
+            DoMouseAction(MouseEventFlags.RIGHTUP);
         }
+
+        #endregion
+
+        #region MiddleMouse
+
+        public static void MiddleClick()
+        {
+            DoMouseAction(MouseEventFlags.MIDDLEDOWN);
+            DoMouseAction(MouseEventFlags.MIDDLEUP);
+        }
+
+        public static void MiddleDown()
+        {
+            DoMouseAction(MouseEventFlags.MIDDLEDOWN);
+        }
+
+        public static void MiddleUp()
+        {
+            DoMouseAction(MouseEventFlags.MIDDLEUP);
+        }
+
+        #endregion
 
         #endregion
     }
