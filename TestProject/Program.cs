42176c4905fc639a761e84186abfdaa5fc14ed7f
Trppyxd <33310226+Trppyxd@users.noreply.github.com>
2019-04-08 21:29:03 +0300

    +Test Project (Is startup project)

---
+++ TestProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InputLibrary;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test application started!");
+
+            Mouse.SetCursorPos(100, 100);
+            Mouse.LeftClick();
+
+            Console.ReadKey();
+        }
+    }
+}
